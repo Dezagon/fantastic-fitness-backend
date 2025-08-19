89713afc346616aa500699333f6ec9c1c13e4a04
Antonio Hernandez <21hernaant@gmail.com>
2025-08-19 12:02:28 -0600

    Initial commit. Models complete

---
+++ models.py
@@ -0,0 +1,31 @@
+from sqlmodel import Field, Relationship, SQLModel
+
+# Attendance
+class Attendance(SQLModel, table=True):
+    class_id: int | None = Field(foreign_key="class.id", primary_key=True)
+    member_id: int | None = Field(foreign_key="member.id", primary_key=True)
+
+# Member
+class Member(SQLModel, table=True):
+    id: int | None = Field(primary_key=True)
+    name: str
+    classes: list["Class"] = Relationship(back_populates="members", link_model=Attendance)
+
+# Trainer
+class Trainer(SQLModel, table=True):
+    id: int | None = Field(primary_key=True)
+    name: str
+    specialty: str
+    classes: list["Class"] = Relationship(back_populates="trainers")
+
+# Class
+class Class(SQLModel, table=True):
+    id: int | None = Field(primary_key=True)
+    name: str
+    trainer_id: int = Field(foreign_key="trainer.id")
+    trainer: Trainer = Relationship(back_populates="classes")
+    member: list[Member] = Relationship(back_populates="classes", link_model=Attendance)
+    date: str
+    duration: int
+
+
